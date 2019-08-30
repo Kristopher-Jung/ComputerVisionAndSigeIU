a732ac56e7a9548e762b3b5ea37be9d779e94db7
khjung <khjung@indiana.edu>
2019-08-30 12:11:44 -0400

    RectangleSelector I and Q

--- Playground/qt_play1.py
+++ Playground/qt_play1.py
@@ -1,55 +1,70 @@
-from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QWidget, QVBoxLayout, QFileDialog, QLabel
-import cv2
-from PyQt5.QtGui import QPixmap
+from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QWidget, QVBoxLayout, QFileDialog
 from scipy import signal
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 from matplotlib.backends.backend_qt5agg import NavigationToolbar2QT as NavigationToolbar
-from matplotlib.widgets import SpanSelector
+from matplotlib.widgets import RectangleSelector
+
 
 class qt_play1(QWidget):
 
     def __init__(self, parent=None):
         super(qt_play1,self).__init__(parent)
         self.fname = None
-        self.figure, self.ax = plt.subplots()
-        self.canvas = FigureCanvas(self.figure)
-        self.toolbar = NavigationToolbar(self.canvas, self)
+        self.figureI, self.axI = plt.subplots(1)
+        self.canvasI = FigureCanvas(self.figureI)
+        self.toolbarI = NavigationToolbar(self.canvasI, self)
+
+        self.figureQ, self.axQ = plt.subplots(1)
+        self.canvasQ = FigureCanvas(self.figureQ)
+        self.toolbarQ = NavigationToolbar(self.canvasQ, self)
+
         win = QMainWindow()
         win.setFixedSize(1280,720)
         menu_widget = QWidget()
         button = QPushButton('read data', menu_widget)
-        horiz_button = QPushButton('horizontal cut', menu_widget)
+        boundIbutton = QPushButton('bounding I data', menu_widget)
+        boundQbutton = QPushButton('bounding Q data', menu_widget)
         layout = QVBoxLayout(menu_widget)
         layout.addWidget(button)
-        layout.addWidget(horiz_button)
-        #self.label = QLabel()
-        #pixmap = QPixmap(self.fname)
-        #self.label.setPixmap(pixmap)
+        layout.addWidget(boundIbutton)
+        layout.addWidget(boundQbutton)
 
         plotWidget = QWidget()
         plotLayout = QVBoxLayout(plotWidget)
-        plotLayout.addWidget(self.toolbar)
-        plotLayout.addWidget(self.canvas)
+        plotLayout.addWidget(self.toolbarI)
+        plotLayout.addWidget(self.canvasI)
+        plotLayout.addWidget(self.toolbarQ)
+        plotLayout.addWidget(self.canvasQ)
 
         win.setMenuWidget(menu_widget)
         win.setCentralWidget(plotWidget)
         button.clicked.connect(self.button_pressed)
+        boundIbutton.clicked.connect(self.boundIbutton_pressed)
+        boundQbutton.clicked.connect(self.boundQbutton_pressed)
 
         win.show()
         app.exit(app.exec_())
 
     def button_pressed(self):
         fname = QFileDialog.getOpenFileName(self,'Open Files','./')
-
-        #self.img = cv2.imread(fname[0])
-        #print(self.img.shape)
         data_IQ_list, I_f, I_t = self.iq_read(fname[0])
         self.plot_I(data_IQ_list, I_f, I_t)
+        self.plot_Q(data_IQ_list, I_f, I_t)
 
-    def horiz_button_pressed(self):
-        print('')
+    def boundIbutton_pressed(self):
+        self.spanI = RectangleSelector(self.axI, self.onselectI, interactive=True)
+
+    def boundQbutton_pressed(self):
+        self.spanQ = RectangleSelector(self.axQ, self.onselectQ, interactive=True)
+
+    def onselectI(self, eclick, erelease):
+        print(eclick, erelease)
+
+    def onselectQ(self, eclick, erelease):
+        print(eclick, erelease)
+
 
     def iq_read(self, file):
         Fs = 1000000
@@ -59,46 +74,28 @@ class qt_play1(QWidget):
         UHF_dat = np.fromfile(file, dtype="float32")
         I = UHF_dat[0::2]
         Q = UHF_dat[1::2]
-        I_f, I_t, I_stft = signal.stft(I, Fs, nperseg=4096)
+        self.I_f, self.I_t, I_stft = signal.stft(I, Fs, nperseg=4096)
         Q_f, Q_t, Q_stft = signal.stft(Q, Fs, nperseg=4096)
         data_IQ_temp.append(np.abs(I_stft))
         data_IQ_temp.append(np.abs(Q_stft))
         data_IQ_list.append(data_IQ_temp)
-        data_IQ_temp = []
-        data_IQ_list = np.array(data_IQ_list)
-        return data_IQ_list, I_f, I_t
+        self.data_IQ_list = np.array(data_IQ_list)
+        return self.data_IQ_list, self.I_f, self.I_t
 
     def plot_I(self,data_IQ_list, I_f, I_t):
-        self.figure.clear()
-        for I in data_IQ_list:
-            self.ax = self.figure.add_subplot(111)
-            self.ax.pcolormesh(I_t, I_f, 10 * np.log10(I[0, :, :]))
-            #ax.title('Inphase STFT Magnitude')
-            #ax.ylabel('Frequency [Hz]')
-            #ax.xlabel('Time [sec]')
-            # set useblit True on gtkagg for enhanced performance
-            figure, ax = plt.subplots()
-            self.span = SpanSelector(self.ax, self.onselect_horiz, 'horizontal', useblit=True,
-                                     rectprops=dict(alpha=0.5, facecolor='red'))
-            self.canvas.draw()
-
-
-
-
-    def onselect_horiz(sekf, xmin, xmax):
-        print(xmin, xmax)
-
-
+        self.figureI.clear()
+        I = data_IQ_list[0]
+        self.axI = self.figureI.add_subplot(111)
+        self.axI.pcolormesh(I_t, I_f, 10 * np.log10(I[0, :, :]))
+        self.canvasI.draw()
 
 
     def plot_Q(self,data_IQ_list,I_f,I_t):
-        for Q in data_IQ_list:
-            plt.pcolormesh(I_t, I_f, 10 * np.log10(Q[1, :, :]))
-            plt.title('Quadrature STFT Magnitude')
-            plt.ylabel('Frequency [Hz]')
-            plt.xlabel('Time [sec]')
-
-
+        self.figureQ.clear()
+        Q = data_IQ_list[0]
+        self.axQ = self.figureQ.add_subplot(111)
+        self.axQ.pcolormesh(I_t, I_f, 10 * np.log10(Q[1, :, :]))
+        self.canvasQ.draw()
 
 
 if __name__ == '__main__':
