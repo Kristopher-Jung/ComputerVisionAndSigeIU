eb19d17955869113e0aa73e272bd763b05514e79
khjung <khjung@indiana.edu>
2019-09-01 22:50:35 -0400

    developed bounding box tool

--- Playground/qt_play1.py
+++ Playground/qt_play1.py
@@ -1,4 +1,4 @@
-from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QWidget, QVBoxLayout, QFileDialog
+from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QWidget, QVBoxLayout, QFileDialog, QHBoxLayout
 from scipy import signal
 import numpy as np
 import matplotlib.pyplot as plt
@@ -11,78 +11,55 @@ class qt_play1(QWidget):
 
     def __init__(self, parent=None):
         super(qt_play1,self).__init__(parent)
+        # initialize GUI stuff
         self.fname = None
+        self.data_IQ_list = None
+        self.I_t = None
+        self.I_f = None
         self.figureI, self.axI = plt.subplots(1)
         self.canvasI = FigureCanvas(self.figureI)
         self.toolbarI = NavigationToolbar(self.canvasI, self)
-
-        self.figureQ, self.axQ = plt.subplots(1)
-        self.canvasQ = FigureCanvas(self.figureQ)
-        self.toolbarQ = NavigationToolbar(self.canvasQ, self)
-
+        self.figure_bounded, self.ax_bounded = plt.subplots(2)
+        self.canvas_bounded = FigureCanvas(self.figure_bounded)
         win = QMainWindow()
-        win.setFixedSize(1280,720)
+        win.setFixedSize(1280,900)
         menu_widget = QWidget()
+
+        #buttons
         button = QPushButton('read data', menu_widget)
         boundIbutton = QPushButton('bounding I data', menu_widget)
-        boundQbutton = QPushButton('bounding Q data', menu_widget)
-        layout = QVBoxLayout(menu_widget)
+        refresh_button = QPushButton('refreseh plot', menu_widget)
+        export_button = QPushButton('export data', menu_widget)
+        metagen_button = QPushButton('gen meta', menu_widget)
+        layout = QHBoxLayout(menu_widget)
         layout.addWidget(button)
         layout.addWidget(boundIbutton)
-        layout.addWidget(boundQbutton)
+        layout.addWidget(refresh_button)
+        layout.addWidget(export_button)
+        layout.addWidget(metagen_button)
+        button.clicked.connect(self.button_pressed)
+        boundIbutton.clicked.connect(self.boundIbutton_pressed)
+        refresh_button.clicked.connect(self.refresh_pressed)
+        export_button.clicked.connect(self.export_pressed)
+        metagen_button.clicked.connect(self.metagen_pressed)
 
+        #plot widgets
         plotWidget = QWidget()
         plotLayout = QVBoxLayout(plotWidget)
         plotLayout.addWidget(self.toolbarI)
         plotLayout.addWidget(self.canvasI)
-        plotLayout.addWidget(self.toolbarQ)
-        plotLayout.addWidget(self.canvasQ)
-
+        plotLayout.addWidget(self.canvas_bounded)
         win.setMenuWidget(menu_widget)
         win.setCentralWidget(plotWidget)
-        button.clicked.connect(self.button_pressed)
-        boundIbutton.clicked.connect(self.boundIbutton_pressed)
-        boundQbutton.clicked.connect(self.boundQbutton_pressed)
-
         win.show()
         app.exit(app.exec_())
 
-    def button_pressed(self):
-        fname = QFileDialog.getOpenFileName(self,'Open Files','./')
-        data_IQ_list, I_f, I_t = self.iq_read(fname[0])
-        self.plot_I(data_IQ_list, I_f, I_t)
-        self.plot_Q(data_IQ_list, I_f, I_t)
 
-    def boundIbutton_pressed(self):
-        self.spanI = RectangleSelector(self.axI, self.onselectI, interactive=True)
-
-    def boundQbutton_pressed(self):
-        self.spanQ = RectangleSelector(self.axQ, self.onselectQ, interactive=True)
-
-    def onselectI(self, eclick, erelease):
-        xmin = eclick.xdata
-        xmax = erelease.xdata
-        ymin = eclick.ydata
-        ymax = erelease.ydata
-        Idata = self.I[0,:,:]
-        xindmin, xindmax = np.searchsorted(self.I_t, (xmin, xmax))
-        yindmin, yindmax = np.searchsorted(self.I_f, (ymin, ymax))
-        #print(Idata[xindmin:xindmax, yindmin:yindmax])
-        print(self.I_t[xindmin:xindmax].shape, self.I_f[yindmin:yindmax].shape, Idata[xindmin:xindmax, yindmin:yindmax].shape)
-        self.figureI.clear()
-        self.axI = self.figureI.add_subplot(111)
-        self.axI.plot(10 * np.log10(Idata[xindmin:xindmax, yindmin:yindmax]))
-        self.canvasI.draw()
-
-    def onselectQ(self, eclick, erelease):
-        print(eclick, erelease)
-
-
+    #read iq data from sigmf data file and saved it to this class
     def iq_read(self, file):
         Fs = 1000000
         data_IQ_list = []
         data_IQ_temp = []
-        #for file in data_files:
         UHF_dat = np.fromfile(file, dtype="float32")
         I = UHF_dat[0::2]
         Q = UHF_dat[1::2]
@@ -94,20 +71,66 @@ class qt_play1(QWidget):
         self.data_IQ_list = np.array(data_IQ_list)
         return self.data_IQ_list, self.I_f, self.I_t
 
-    def plot_I(self,data_IQ_list, I_f, I_t):
+    #read sigmf data file
+    def button_pressed(self):
+        fname = QFileDialog.getOpenFileName(self,'Open Files','./')
+        try:
+            self.data_IQ_list, self.I_f, self.I_t = self.iq_read(fname[0])
+            self.plot_I_initial(self.data_IQ_list, self.I_f, self.I_t)
+        except FileNotFoundError:
+            return
+
+    #this is called when bbox pressed
+    def boundIbutton_pressed(self):
+        self.spanI = RectangleSelector(self.axI, self.onselectI, interactive=True)
+
+    #this is called when refreseh pressed
+    def refresh_pressed(self):
+        if (self.data_IQ_list is not None) and (self.I_f is not None) and (self.I_t is not None):
+            self.plot_I_initial(self.data_IQ_list, self.I_f, self.I_t)
+            self.figure_bounded.clear()
+
+    #this is called when export data pressed
+    def export_pressed(self):
+        print('not implemented yet')
+
+    #this is called to generate new custom metadata sigmf
+    def metagen_pressed(self):
+        print('not implemented yet')
+
+    #this is called for bounding box region, and its data
+    def onselectI(self, eclick, erelease):
+        xmin = eclick.xdata
+        xmax = erelease.xdata
+        ymin = eclick.ydata
+        ymax = erelease.ydata
+        Idata = self.I[0,:,:]
+        Qdata = self.I[1,:,:]
+        xindmin, xindmax = np.searchsorted(self.I_t, (xmin, xmax))
+        yindmin, yindmax = np.searchsorted(self.I_f, (ymin, ymax))
+        print('I_t X Index Min to max = ', self.I_t[xindmin:xindmax].shape)
+        print('I_t X index min = ', xindmin, ' xindex max = ', xindmax)
+        print('I_f y Index min to max = ', self.I_f[yindmin:yindmax].shape)
+        print('I_f y index min = ', yindmin, ' y index max = ', yindmax)
+        print('I data x min_to_max, y min_to_max = ', Idata[xindmin:xindmax, yindmin:yindmax].shape)
+        self.figure_bounded.clear()
+        self.ax_bounded = self.figure_bounded.subplots(2)
+        self.ax_bounded[0].plot(10 * np.log10(Idata[xindmin:xindmax, yindmin:yindmax]))
+        self.ax_bounded[0].set_title('I-region')
+        self.ax_bounded[1].plot(10 * np.log10(Qdata[xindmin:xindmax, yindmin:yindmax]))
+        self.ax_bounded[1].set_title('Q-region')
+        self.canvas_bounded.draw()
+
+    #plotting Initial Inphase data
+    def plot_I_initial(self,data_IQ_list, I_f, I_t):
         self.figureI.clear()
         self.I = data_IQ_list[0]
         self.axI = self.figureI.add_subplot(111)
         self.axI.pcolormesh(I_t, I_f, 10 * np.log10(self.I[0, :, :]))
+        self.axI.set_xlabel('Time [sec]')
+        self.axI.set_ylabel('Frequency [Hz]')
+        self.axI.set_title('Inphase STFT Magnitude')
         self.canvasI.draw()
-
-
-    def plot_Q(self,data_IQ_list,I_f,I_t):
-        self.figureQ.clear()
-        self.Q = data_IQ_list[0]
-        self.axQ = self.figureQ.add_subplot(111)
-        self.axQ.pcolormesh(I_t, I_f, 10 * np.log10(self.Q[1, :, :]))
-        self.canvasQ.draw()
 
 
 if __name__ == '__main__':
---
+++ serious/bbox_tool_main.py
@@ -0,0 +1,136 @@
+import cv2
+from process_unit import process_unit
+import json
+
+all_boxes = {}
+refPt = []
+mouse_clicked = False
+loaded = False
+
+def load_boxes_from_json():
+    global all_boxes, loaded
+    try:
+        with open('./boxes.json') as f:
+            dictdumnp = json.loads(f.read())
+        all_boxes = dictdumnp
+        print(all_boxes)
+        loaded = True
+    except:
+        print('no local json detected')
+
+def mouse_event(event, x, y, flags, param):
+    global refPt, mouse_clicked
+    # if the left mouse button was clicked, record the starting
+    # (x, y) coordinates and indicate that cropping is being
+    # performed
+    if event == cv2.EVENT_LBUTTONDOWN:
+        refPt = [(x, y)]
+        mouse_clicked = True
+
+    # check to see if the left mouse button was released
+    elif event == cv2.EVENT_LBUTTONUP:
+        # record the ending (x, y) coordinates and indicate that
+        # the cropping operation is finished
+        refPt.append((x, y))
+        all_boxes[curr_path].append(refPt)
+        mouse_clicked = False
+        # draw a rectangle around the region of interest
+        box = cv2.rectangle(curr_image, refPt[0], refPt[1], (255, 0, 0), 1)
+        index = all_boxes[curr_path].__len__()-1
+        cv2.putText(box, str(index), refPt[0], cv2.FONT_HERSHEY_SIMPLEX, 0.7, (100, 255, 100), 2)
+        cv2.imshow("image", curr_image)
+        refPt = []
+        print(all_boxes)
+
+    elif event == cv2.EVENT_MOUSEMOVE:
+        if mouse_clicked:
+            orig = curr_image.copy()
+            cv2.rectangle(orig, refPt[0], (x, y), (255, 0, 0), 1)
+            cv2.imshow("image", orig)
+
+
+if __name__ == '__main__':
+    # initialize
+    pu = process_unit()
+    pu.read_frames()
+    frames = pu.frames
+    outer = True
+    frame_index = 0
+
+    # load local json file if there is one
+    load_boxes_from_json()
+    while frame_index < frames.__len__():
+        if (frame_index < 0):
+            frame_index = 0
+        frame = frames[frame_index]
+        if not outer:
+            break
+        while True:
+            curr_path = frame
+            print(curr_path)
+            curr_image = cv2.imread(curr_path)
+            initial = curr_image.copy()
+            if loaded:
+                if curr_path in all_boxes:
+                    prebounds = all_boxes[curr_path]
+                    box_index = 0
+                    for bound in prebounds:
+                        x = bound[0]
+                        y = bound[1]
+                        box = cv2.rectangle(curr_image, (x[0], x[1]), (y[0], y[1]), (0, 0, 255), 2)
+                        cv2.putText(box, str(box_index), (x[0], x[1]), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (100,255,100), 2)
+                        box_index+=1
+                cv2.imshow("image", curr_image)
+            else:
+                all_boxes[frame] = []
+                cv2.imshow('image', curr_image)
+            cv2.setMouseCallback('image', mouse_event)
+
+            #key strokes
+            k = cv2.waitKey(0)
+            #next frame
+            if '.' == chr(k & 255):
+                frame_index+=1
+                break
+            #previous frame
+            elif ',' == chr(k & 255):
+                frame_index -= 1
+                break
+            #save bounding boxes
+            elif 's' == chr(k & 255):
+                with open('boxes.json', 'w') as f:
+                    json.dump(all_boxes, f)
+
+            #refresh current frame
+            elif 'r' == chr(k & 255):
+                all_boxes[frame] = []
+                cv2.imshow('image', initial)
+
+            #delete single bounding box from this frame
+            elif 'd' == chr(k & 255):
+                bi = input("Enter Number Of Bounding Box you want to delete: ")
+                curr_box = all_boxes[frame]
+                del curr_box[int(bi)]
+                all_boxes[frame] = curr_box
+                prebounds = all_boxes[frame]
+                box_index = 0
+                for bound in prebounds:
+                    x = bound[0]
+                    y = bound[1]
+                    box = cv2.rectangle(curr_image, (x[0], x[1]), (y[0], y[1]), (0, 0, 255), 2)
+                    cv2.putText(box, str(box_index), (x[0], x[1]), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (100, 255, 100), 2)
+                    box_index += 1
+                cv2.imshow('image', initial)
+
+            #refresh all frames
+            elif 'f' == chr(k & 255):
+                all_boxes = {}
+                cv2.imshow('image', initial)
+
+            #quit program
+            elif 'q' == chr(k & 255):
+                cv2.destroyAllWindows()
+                outer = False
+                break
+
+
